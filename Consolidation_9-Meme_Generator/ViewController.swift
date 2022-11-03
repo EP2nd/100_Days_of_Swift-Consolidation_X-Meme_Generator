4f132f6af336ef28502ff34c2a8fbc834ddac024
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-11-03 02:37:03 +0100

    Completion
    The app misbehaves: I left the best result of positioning captions I could set, but the lines are way too narrow. Also, the sharing functionality extracts a black rectangle. I did all I could at the moment to spot the cause, but I failed. I hope I will be able to fix it in the future.

--- Consolidation_9-Meme_Generator/ViewController.swift
+++ Consolidation_9-Meme_Generator/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     
+    var shareButton: UIBarButtonItem!
+    var editButton: UIBarButtonItem!
+    
+    var topLineText: String?
+    var bottomLineText: String?
+    var currentImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,8 +27,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         let importButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addImage))
         navigationItem.rightBarButtonItems = [spacer, importButton]
         
-        let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareImage))
-        let editButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editImage))
+        shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareImage))
+        editButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editImage))
+        shareButton.isEnabled = false
+        editButton.isEnabled = false
         toolbarItems = [shareButton, spacer, editButton]
         navigationController?.isToolbarHidden = false
         
@@ -40,14 +49,72 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         dismiss(animated: true)
         
         imageView.image = image
+        currentImage = image
+        shareButton.isEnabled = true
+        editButton.isEnabled = true
     }
     
     @objc func shareImage() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else { return }
         
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     @objc func editImage() {
+        let ac = UIAlertController(title: "Top and bottom lines", message: "Please type in top and bottom line texts. If you need only one of the lines, please leave the other one blank.", preferredStyle: .alert)
+        ac.addTextField()
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.addAction(UIAlertAction(title: "Save", style: .default) { [weak self, weak ac] _ in
+            let newTopLineText = ac?.textFields?[0].text ?? ""
+            let newBottomLineText = ac?.textFields?[1].text ?? ""
+            
+            self?.topLineText = newTopLineText
+            self?.bottomLineText = newBottomLineText
+            
+            self?.drawMeme()
+        })
+        present(ac, animated: true)
+    }
+    
+    func drawMeme() {
+        guard let image = currentImage else { return }
         
+        let renderer = UIGraphicsImageRenderer(size: image.size)
+        
+        let meme = renderer.image { ctx in
+            image.draw(at: CGPoint(x: 0, y: 0))
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let fontScale = CGFloat((image.size.width + image.size.height) / 30)
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont.boldSystemFont(ofSize: fontScale),
+                .foregroundColor: UIColor.white,
+                .strokeColor: UIColor.black,
+                .strokeWidth: -2,
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            let topLineString = topLineText
+            let bottomLineString = bottomLineText
+            
+            let attributedTopString = NSAttributedString(string: topLineString!, attributes: attributes)
+            let attributedBottomString = NSAttributedString(string: bottomLineString!, attributes: attributes)
+            
+            let stringWidth = Int((image.size.width) / 6) * 2
+            let textX = Int(image.size.width / 3)
+            let topTextY = Int(image.size.height / 8)
+            let bottomTextY = Int((image.size.height / 8) * 7)
+            
+            attributedTopString.draw(with: CGRect(x: textX, y: topTextY, width: stringWidth, height: 100), context: nil)
+            attributedBottomString.draw(with: CGRect(x: textX, y: bottomTextY, width: stringWidth, height: 100), context: nil)
+        }
+        imageView.image = meme
     }
     
     func drawWelcomeScreen() {
